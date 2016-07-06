d7b8237aa75091521cf7c27881a157c8070cd24d
Ilinca Vamesu <ivamesu@endava.com>
2016-07-06 14:42:00 +0300

    Second try

---
+++ First/src/SecondClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ivamesu on 7/6/2016.
+ */
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Pentru inca un branch");
+    }
+}
